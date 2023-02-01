b147a287652e5551ab60f20f6041d916eabaeea7
Bojan0611 <jbojanowski11@gmail.com>
2023-02-01 23:01:40 +0100

    overwrite str method in Book model

--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -3,3 +3,7 @@ from django.db import models
 class Book(models.Model):
     title = models.CharField(max_length=100)
     rating = models.IntegerField()
+    
+    def __str__(self):
+        return f"{self.title} ({self.rating})"
+    
