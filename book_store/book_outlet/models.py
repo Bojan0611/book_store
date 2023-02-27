ba4cbe347442d88d1090c93dad0dc60701474377
Bojan0611 <jbojanowski11@gmail.com>
2023-02-27 23:13:57 +0100

    admin panel

--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.urls import reverse
+from django.utils.text import slugify
 
 class Book(models.Model):
     title = models.CharField(max_length=100)
@@ -9,9 +10,13 @@ class Book(models.Model):
         )
     author = models.CharField(null=True, max_length=100)
     is_bestselling = models.BooleanField(default=False)
+    slug = models.SlugField(default="",
+                            blank=True,
+                            null=True,
+                            db_index=True) # Harry Potter 1 -> harry-potter-1
     
     def get_absolute_url(self):
-        return reverse("book-detail", args=[self.id])
+        return reverse("book-detail", args=[self.slug])
 
     def __str__(self):
         return f"{self.title} ({self.rating})"
